3e1724000b943eee25047517c91870a9a8dd6abf
yoyojambo <omar533330@gmail.com>
2019-12-20 06:13:27 -0600

    Just need to make the function that validates a move by a piece for its type

--- ConChess.py
+++ ConChess.py
@@ -38,10 +38,13 @@ class chess(object):
         else:
             self.move = input("Black movement: ")
         
+        if self.move == "exit":
+            quit()
+
         if ord(self.move[0]) > 104 or ord(self.move[0]) < 97:
             print("That is not admitted, try again")
             self.takeinput()
-        elif ord(self.move[3]) > 104 or ord(self.move[3]) < 97:
+        if ord(self.move[3]) > 104 or ord(self.move[3]) < 97:
             print("That is not admitted, try again")
             self.takeinput()
 
@@ -49,14 +52,25 @@ class chess(object):
             self.origin = [8 - int(self.move[1]) , ord(self.move[0].lower()) - 97]
             self.destiny = [8 - int(self.move[4]) , ord(self.move[3].lower()) - 97]
 
-    def movepiece(self):
-        self.move[self.destiny] = self.move[self.origin]
-        self.move[self.origin] = ''
-        self.turn = False
+    def movepiece(self, o, d):
+        if self.board[d[0]][d[1]].isupper != self.board[o[0]][o[1]].isupper:
+            if self.pieceallowedmove(self.board[d[0]][d[1]], self.board[o[0]][o[1]]):
+                self.board[d[0]][d[1]] = self.board[o[0]][o[1]]
+                self.board[o[0]][o[1]] = ''
+                self.turn = not self.turn
+            else:
+                print("That is not an allowed move for that piece.")
+            
+        else:
+            print("You can't eat you own piece.")
+            self.takeinput()
+        
 
 if __name__ == "__main__":
     chess = chess()
-    chess.draw()
-    chess.takeinput()
-    chess.movepiece()
-    chess.draw()
+    while True:
+        chess.draw()
+        chess.takeinput()
+        chess.movepiece(chess.origin, chess.destiny)
+    
+    
